ac74a1e8f756f9c29e5e39db327e02cb734c0258
dscaffo <dscaffo@gmail.com>
2021-04-23 00:27:22 -0300

    SetUp dependencies

---
+++ server/BetterCalm/Injector/Injector.cs
@@ -0,0 +1,35 @@
+﻿using BL;
+using BLInterfaces;
+using DataAccess.Context;
+using DataAccess.Repositories;
+using DataAccessInterfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Injector
+{
+	public class ServicesInjector
+	{
+		private readonly IServiceCollection services;
+
+		public ServicesInjector(IServiceCollection services)
+		{
+			this.services = services;
+		}
+
+		public void AddBLServices() 
+		{
+			services.AddScoped<IMediaPlayer, MediaPlayer>();
+		}
+
+		public void AddDataAccessServices()
+		{
+			services.AddScoped<IPlaylistRepository, PlaylistRepository>();
+		}
+
+		public void AddContextServices()
+		{
+			services.AddScoped<DbContext, BetterCalmContext>();
+		}
+	}
+}
--- server/BetterCalm/WebAPI/Startup.cs
+++ server/BetterCalm/WebAPI/Startup.cs
@@ -1,15 +1,9 @@
+using Injector;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI
 {
@@ -26,6 +20,10 @@ namespace WebAPI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+			ServicesInjector servicesInjector = new ServicesInjector(services);
+			servicesInjector.AddBLServices();
+			servicesInjector.AddContextServices();
+			servicesInjector.AddDataAccessServices();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
