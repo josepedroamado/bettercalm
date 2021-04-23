0d7c4c925b29925cfc93c236baf98a3635120044
dscaffo <dscaffo@gmail.com>
2021-04-22 22:46:54 -0300

    Add PlaylistRepository with the Get method (not implemented)

---
+++ server/BetterCalm/DataAccess/Repositories/PlaylistRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Repositories
+{
+	public class PlaylistRepository : IPlaylistRepository
+	{
+		private readonly DbContext context;
+		private readonly DbSet<Playlist> playlists;
+
+		public PlaylistRepository(DbContext context)
+		{
+			this.context = context;
+			this.playlists = context.Set<Playlist>();
+		}
+
+		public IEnumerable<Playlist> Get()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ server/BetterCalm/DataAccessInterfaces/IPlaylistRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using System.Collections.Generic;
+
+namespace DataAccessInterfaces
+{
+    public interface IPlaylistRepository
+    {
+        IEnumerable<Playlist> Get();
+    }
+}
