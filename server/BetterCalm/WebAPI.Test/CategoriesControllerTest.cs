073fc8d40387e63a7ab39f5a2a84bd6873544cf0
pepe16694 <jose.pedro.amado@gmail.com>
2021-04-24 17:08:49 -0300

    Created testing class for CategoriesController.

---
+++ server/BetterCalm/WebAPI.Test/CategoriesControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPI.Test
+{
+    [TestClass]
+    public class CategoriesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            
+        }
+    }
+}
