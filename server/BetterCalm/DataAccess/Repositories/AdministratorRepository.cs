d01f158e0b7917a3aec5efcb95efa0242e8c75af
dscaffo <dscaffo@gmail.com>
2021-04-25 19:36:01 -0300

    Added administrator repository (Not implemented)

---
+++ server/BetterCalm/DataAccess/Repositories/AdministratorRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess.Repositories
+{
+	public class AdministratorRepository : IAdministratorRepository
+	{
+		private DbContext context;
+		private DbSet<Administrator> administrators;
+
+		public AdministratorRepository(DbContext context)
+		{
+			this.context = context;
+			this.administrators = context.Set<Administrator>();
+		}
+
+
+		public Administrator Get(string eMail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
