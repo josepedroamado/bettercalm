fdc4c2eee02cb5f155c0291d44cca0e0692541b5
dscaffo <dscaffo@gmail.com>
2021-04-26 22:05:45 -0300

    Added get method to contents controller

--- server/BetterCalm/WebAPI/Controllers/ContentsController.cs
+++ server/BetterCalm/WebAPI/Controllers/ContentsController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLInterfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +10,15 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class ContentsController : ControllerBase
 	{
-		// GET: api/<ContentsController>
+		public ContentsController(IContentPlayer contentPlayerLogic)
+		{
+
+		}
+
 		[HttpGet]
-		public IEnumerable<string> Get()
+		public IActionResult Get()
 		{
-			return new string[] { "value1", "value2" };
-		}
-
-		// GET api/<ContentsController>/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
-
-		// POST api/<ContentsController>
-		[HttpPost]
-		public void Post([FromBody] string value)
-		{
-		}
-
-		// PUT api/<ContentsController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
-
-		// DELETE api/<ContentsController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
+			throw new NotImplementedException();
 		}
 	}
 }
