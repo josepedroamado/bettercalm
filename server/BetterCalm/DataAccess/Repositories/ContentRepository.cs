a5a95dbcc6e18fb17ba1216e9638f8a74a967789
dscaffo <dscaffo@gmail.com>
2021-04-26 21:07:37 -0300

    Add ContentRepository as needed to create a failing test (DataAccess, not implemented)

---
+++ server/BetterCalm/DataAccess/Repositories/ContentRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Repositories
+{
+	public class ContentRepository : IContentRepository
+	{
+
+		public ContentRepository(DbContext context)
+		{
+
+		}
+		public IEnumerable<Content> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
