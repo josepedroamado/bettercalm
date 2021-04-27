bce0ceec8324dcb86bdf6cf9b2d8084ca488a178
dscaffo <dscaffo@gmail.com>
2021-04-26 22:18:33 -0300

    Implemented get all contents (WebAPI)

--- server/BetterCalm/WebAPI/Controllers/ContentsController.cs
+++ server/BetterCalm/WebAPI/Controllers/ContentsController.cs
@@ -1,6 +1,8 @@
 ﻿using BLInterfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
+using Model;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,15 +12,21 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class ContentsController : ControllerBase
 	{
+		private readonly IContentPlayer contentPlayerLogic;
+
 		public ContentsController(IContentPlayer contentPlayerLogic)
 		{
-
+			this.contentPlayerLogic = contentPlayerLogic;
 		}
 
 		[HttpGet]
 		public IActionResult Get()
 		{
-			throw new NotImplementedException();
+			IEnumerable<ContentBasicInfo> contents =
+				this.contentPlayerLogic.GetContents().
+				Select(content => new ContentBasicInfo(content));
+
+			return Ok(contents);
 		}
 	}
 }
