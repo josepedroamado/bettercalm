2968f1663cbde5fd5608c57f324be0f72255c122
pepe16694 <jose.pedro.amado@gmail.com>
2021-04-28 12:58:29 -0300

    Forgot to stage files. Again...

---
+++ server/BetterCalm/BL/IllnessLogic.cs
@@ -0,0 +1,22 @@
+﻿using BLInterfaces;
+using DataAccessInterfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BL
+{
+    public class IllnessLogic : IIllnessLogic
+    {
+        private readonly IIllnessRepository illnessRepository;
+
+        public IllnessLogic(IIllnessRepository illnessRepository)
+        {
+            this.illnessRepository = illnessRepository;
+        }
+        public IEnumerable<Illness> GetIllnesses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
