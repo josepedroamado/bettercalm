0babaccf2aaff4a43fc9b42aa508c29f5f4ac721
pepe16694 <jose.pedro.amado@gmail.com>
2021-04-28 12:43:18 -0300

    Added test class and test method to test GetIllnesses. Also created needed classes and methods. (DataAccess)

---
+++ server/BetterCalm/DataAccess/Repositories/IllnessRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    public class IllnessRepository : IIllnessRepository
+    {
+        private DbContext context;
+
+        public IllnessRepository(DbContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Illness> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
