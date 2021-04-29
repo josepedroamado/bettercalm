d5117db7fd4f6fc8b167e3d7d23cc32eca82adaa
dscaffo <dscaffo@gmail.com>
2021-04-28 21:21:58 -0300

    Adde to entity method to contentmodel (WebAPI)

--- server/BetterCalm/Model/ContentModel.cs
+++ server/BetterCalm/Model/ContentModel.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using Domain;
+using System;
+using System.Collections.Generic;
 
 namespace Model
 {
 	public class ContentModel
 	{
+		public int Id { get; set; }
 		public string Name { get; set; }
 		public string ContentLength { get; set; }
 		public string ArtistName { get; set; }
-		public string ImageURL { get; set; }
-		public string AudioURL { get; set; }
+		public string ImageUrl { get; set; }
+		public string AudioUrl { get; set; }
 		public int[] Categories { get; set; }
 		public IEnumerable<PlaylistBasicInfo> Playlists {get; set;}
+
+		public Content ToEntity()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
