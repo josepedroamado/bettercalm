c14fc12a02ac99d2548e65bdd2615ff0873b64bf
dscaffo <dscaffo@gmail.com>
2021-04-30 18:04:33 -0300

    Added update content tests at BL level

--- server/BetterCalm/BL.Test/ContentLogicTest.cs
+++ server/BetterCalm/BL.Test/ContentLogicTest.cs
@@ -582,5 +582,336 @@ namespace BL.Test
 			contentRepositoryMock.VerifyAll();
 			Assert.AreNotEqual(toDeleteContent, obtainedContent);
 		}
+
+		//////////////////////
+		//////////////////////
+		//////////////////////
+		/////////////////////
+		///
+		[TestMethod]
+		public void UpdateContentWithNewPlaylist()
+		{
+			Category music = new Category()
+			{
+				Id = 1,
+				Name = "Musica"
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1,
+				Name = "Besto of Bon Jovi"
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				Categories = new List<Category>(){
+						music
+					},
+				PlayLists = new List<Playlist>()
+				{
+					playlist
+				},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+
+			contentRepositoryMock.Setup(m => m.Update(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			playlistRepository.Setup(m => m.Get(playlist.Id)).Throws(new NotFoundException(playlist.Id.ToString()));
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.PlayLists.Append(new Playlist()
+			{
+				Id = 2,
+				Name = "Playlist2"
+			});
+			contentLogic.UpdateContent(toSaveContent);
+			
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreEqual(toSaveContent, obtainedContent);
+		}
+
+		[TestMethod]
+		public void UpdateContentWithExistentPlaylist()
+		{
+			Category music = new Category()
+			{
+				Id = 1,
+				Name = "Musica"
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1,
+				Name = "Besto of Bon Jovi"
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				Categories = new List<Category>(){
+						music
+					},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Update(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			playlistRepository.Setup(m => m.Get(playlist.Id)).Returns(playlist);
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.PlayLists.Append(playlist);
+			contentLogic.UpdateContent(toSaveContent);
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreEqual(toSaveContent, obtainedContent);
+		}
+
+		[TestMethod]
+		public void UpdateContentWithoutPlaylist()
+		{
+			Category music = new Category()
+			{
+				Id = 1,
+				Name = "Musica"
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1,
+				Name = "Besto of Bon Jovi"
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				Categories = new List<Category>(){
+						music
+					},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Update(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.PlayLists = new List<Playlist>();
+			contentLogic.UpdateContent(toSaveContent);
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreEqual(toSaveContent, obtainedContent);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(NotFoundException))]
+		public void UpdateContentInvalidCategory()
+		{
+			Category music = new Category()
+			{
+				Id = 1,
+				Name = "Musica"
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1,
+				Name = "Besto of Bon Jovi"
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				Categories = new List<Category>(){
+						music
+					},
+				PlayLists = new List<Playlist>()
+				{
+					playlist
+				},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			playlistRepository.Setup(m => m.Get(playlist.Id)).Returns(playlist);
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+			Category invalid = new Category()
+			{
+				Id = 2222,
+				Name = "Invalid category"
+			};
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(invalid);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.Categories.Append(invalid);
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreNotEqual(toSaveContent, obtainedContent);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(UnableToCreatePlaylistException))]
+		public void UpdateContentUnableToCreatePlaylist()
+		{
+			Category music = new Category()
+			{
+				Id = 1,
+				Name = "Musica"
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				Categories = new List<Category>(){
+						music
+					},
+				PlayLists = new List<Playlist>()
+				{
+					playlist
+				},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			Playlist notFound = new Playlist()
+			{
+				Id = 2
+			};
+			playlistRepository.Setup(m => m.Get(playlist.Id)).Returns(notFound);
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.PlayLists.Append(notFound);
+			contentLogic.UpdateContent(toSaveContent);
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreNotEqual(obtainedContent, toSaveContent);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(MissingCategoriesException))]
+		public void UpdateContentMissingCategories()
+		{
+			Category music = new Category()
+			{
+				Id = 1
+			};
+
+			Playlist playlist = new Playlist()
+			{
+				Id = 1,
+				Name = "Besto of Bon Jovi"
+			};
+
+			Content toSaveContent = new Content()
+			{
+				ArtistName = "Bon Jovi",
+				PlayLists = new List<Playlist>()
+				{
+					playlist
+				},
+				Id = 1,
+				ContentLength = new TimeSpan(0, 2, 30),
+				Name = "It's My Life",
+				ImageUrl = "http://www.images.com/image.jpg",
+				AudioUrl = "http://www.audios.com/audio.mp3"
+			};
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(toSaveContent));
+			contentRepositoryMock.Setup(m => m.Get(toSaveContent.Id)).Returns(toSaveContent);
+
+			Mock<IPlaylistRepository> playlistRepository = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			playlistRepository.Setup(m => m.Get(playlist.Id)).Returns(playlist);
+
+			Mock<ICategoryRepository> categoryRepository = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepository.Setup(m => m.Get(music.Id)).Returns(music);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepository.Object, categoryRepository.Object);
+
+			contentLogic.CreateContent(toSaveContent);
+			toSaveContent.Categories = new List<Category>();
+			contentLogic.UpdateContent(toSaveContent);
+			Content obtainedContent = contentLogic.GetContent(toSaveContent.Id);
+
+			contentRepositoryMock.VerifyAll();
+			Assert.AreNotEqual(obtainedContent, toSaveContent);
+		}
 	}
 }
