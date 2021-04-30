82609f3833de14103c9a0b832d3617d3badd0344
dscaffo <dscaffo@gmail.com>
2021-04-30 01:16:33 -0300

    Added content comparer

---
+++ server/BetterCalm/Domain/ContentComparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Domain
+{
+	public class ContentComparer : IEqualityComparer<Content>
+	{
+		public bool Equals([DisallowNull] Content xContent, [DisallowNull] Content yContent)
+		{
+			bool equalsPlaylist;
+			if (xContent.PlayLists != null && yContent.PlayLists != null)
+				equalsPlaylist = xContent.PlayLists.SequenceEqual(yContent.PlayLists);
+			else
+				equalsPlaylist = true;
+
+			bool equalsCategories;
+			if (xContent.Categories != null && yContent.Categories != null)
+				equalsCategories = xContent.Categories.SequenceEqual(yContent.Categories);
+			else
+				equalsCategories = true;
+
+			return xContent.ArtistName.Equals(yContent.ArtistName) &&
+				xContent.AudioUrl.Equals(yContent.AudioUrl) &&
+				xContent.ContentLength.Equals(yContent.ContentLength) &&
+				xContent.Id == yContent.Id &&
+				xContent.ImageUrl.Equals(yContent.ImageUrl) &&
+				xContent.Name.Equals(yContent.Name) &&
+				equalsPlaylist && equalsCategories;
+		}
+
+		public int GetHashCode([DisallowNull] Content obj)
+		{
+			return HashCode.Combine(obj.Name, obj.ArtistName, obj.AudioUrl);
+		}
+	}
+}
