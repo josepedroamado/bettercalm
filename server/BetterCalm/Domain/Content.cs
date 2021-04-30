524c09c54488a08353541ae5708f96a41560f551
dscaffo <dscaffo@gmail.com>
2021-04-30 00:25:53 -0300

    Minor fix for update from content (Domain)

--- server/BetterCalm/Domain/Content.cs
+++ server/BetterCalm/Domain/Content.cs
@@ -16,14 +16,22 @@ namespace Domain
 
 		public void UpdateFromContent(Content content)
 		{
-			this.ArtistName = content.ArtistName;
-			this.AudioUrl = content.AudioUrl;
-			this.Categories = content.Categories;
-			this.ContentLength = content.ContentLength;
 			this.Id = content.Id;
-			this.ImageUrl = content.ImageUrl;
-			this.Name = content.Name;
-			this.PlayLists = content.PlayLists;
+
+			if (!string.IsNullOrEmpty(content.Name))
+				this.ArtistName = content.ArtistName;
+			if (!string.IsNullOrEmpty(content.AudioUrl))
+				this.AudioUrl = content.AudioUrl;
+			if (content.Categories != null)
+				this.Categories = content.Categories;
+			if (content.ContentLength != null)
+				this.ContentLength = content.ContentLength;
+			if (!string.IsNullOrEmpty(content.ImageUrl))
+				this.ImageUrl = content.ImageUrl;
+			if (!string.IsNullOrEmpty(content.Name))
+				this.Name = content.Name;
+			if (content.PlayLists != null)
+				this.PlayLists = content.PlayLists;
 		}
 	}
 }
