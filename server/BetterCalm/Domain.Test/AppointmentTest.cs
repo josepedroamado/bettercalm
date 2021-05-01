153bffc8eaf679b526431e79a915fb923bddd3ae
dscaffo <dscaffo@gmail.com>
2021-05-01 20:40:52 -0300

    Added test case for appointment get date

---
+++ server/BetterCalm/Domain.Test/AppointmentTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Domain.Test
+{
+	[TestClass]
+	public class AppointmentTest
+	{
+		[TestMethod]
+		public void GetDateOk()
+		{
+			DateTime date = DateTime.Now;
+
+			Appointment appointment = new Appointment()
+			{
+				Id = 1,
+				Date = date
+			};
+
+			Assert.AreEqual(date.Date, appointment.GetDate());
+		}
+	}
+}
