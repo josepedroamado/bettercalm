33a44097b38975deb968b94182aab20f4c57045e
dscaffo <dscaffo@gmail.com>
2021-05-01 20:34:17 -0300

    Added GetScheduleDate method (not implemented)

--- server/BetterCalm/Domain/Schedule.cs
+++ server/BetterCalm/Domain/Schedule.cs
@@ -5,9 +5,19 @@ namespace Domain
 {
 	public class Schedule
 	{
+		private ICollection<Appointment> appointments = new List<Appointment>();
 		public int Id { get; set; }
 		public Psychologist Psychologist { get; set; }
 		public DateTime Date { get; set; }
-		public ICollection<Appointment> Appointments { get; set; }
+		public ICollection<Appointment> Appointments
+		{
+			get => appointments;
+			set => appointments = value;
+		}
+
+		public DateTime GetScheduleDate()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
