b229364a9d179d8b4eada399e3ae5eaf93de584c
dscaffo <dscaffo@gmail.com>
2021-05-01 14:38:39 -0300

    Added appointment logic with create appointment method (not implemented - BL)

---
+++ server/BetterCalm/BL/AppointmentLogic.cs
@@ -0,0 +1,19 @@
+﻿using BLInterfaces;
+using DataAccessInterfaces;
+using Domain;
+
+namespace BL
+{
+	public class AppointmentLogic : IAppointmentLogic
+	{
+		public AppointmentLogic(IPsychologistRepository psychologistRepository, IIllnessRepository illnessRepository)
+		{
+
+		}
+
+		public Appointment CreateAppointment(Patient patient, Illness illness)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
