508591c2c47846dd5dc66429266de819f8a01f13
dscaffo <dscaffo@gmail.com>
2021-05-01 14:26:37 -0300

    Added Patient Repository and GetPatient Method

---
+++ server/BetterCalm/DataAccess/Repositories/PatientRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+	public class PatientRepository : IPatientRepository
+	{
+		public Patient GetPatient(string eMail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ server/BetterCalm/DataAccessInterfaces/IPatientRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace DataAccessInterfaces
+{
+	public interface IPatientRepository
+	{
+		Patient GetPatient(string eMail);
+	}
+}
