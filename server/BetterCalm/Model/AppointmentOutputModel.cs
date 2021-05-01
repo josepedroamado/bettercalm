644b4367898928c19d447cbefe089020dafcaf71
dscaffo <dscaffo@gmail.com>
2021-05-01 17:15:33 -0300

    Added new constructor to output model

--- server/BetterCalm/Model/AppointmentOutputModel.cs
+++ server/BetterCalm/Model/AppointmentOutputModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Domain;
+using System;
 
 namespace Model
 {
 	public class AppointmentOutputModel
 	{
+		public AppointmentOutputModel() { }
+		public AppointmentOutputModel(Appointment appointment)
+		{
+			this.PsychologistName = appointment.Psychologist.GetFullName();
+			this.Format = appointment.Psychologist.Format.ToString();
+			this.Address = appointment.Address;
+			this.Date = appointment.Date;
+		}
+
 		public string PsychologistName { get; set; }
 		public string Format { get; set; }
 		public string Address { get; set; }
 		public DateTime Date { get; set; }
+
 	}
 }
