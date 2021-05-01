a0c5b02012157baf35430027663603377718de3b
dscaffo <dscaffo@gmail.com>
2021-05-01 20:34:28 -0300

    Added test case for get schedule date

---
+++ server/BetterCalm/Domain.Test/ScheduleTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Domain.Test
+{
+	[TestClass]
+	public class ScheduleTest
+	{
+		[TestMethod]
+		public void GetDateOk()
+		{
+			DateTime date = DateTime.Now;
+			Schedule schedule = new Schedule()
+			{
+				Id = 1,
+				Date = date
+			};
+
+			Assert.AreEqual(date.Date, schedule.GetScheduleDate());
+		}
+	}
+}
