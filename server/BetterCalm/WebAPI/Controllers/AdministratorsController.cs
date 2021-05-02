5785278182173baeb714f3d01a86bfe6a7b0d498
dscaffo <dscaffo@gmail.com>
2021-05-01 23:32:14 -0300

    Added post method to administrator controller

--- server/BetterCalm/WebAPI/Controllers/AdministratorsController.cs
+++ server/BetterCalm/WebAPI/Controllers/AdministratorsController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers
 {
@@ -12,36 +8,11 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class AdministratorsController : ControllerBase
 	{
-		// GET: api/<AdministratorsController>
-		[HttpGet]
-		public IEnumerable<string> Get()
-		{
-			return new string[] { "value1", "value2" };
-		}
-
-		// GET api/<AdministratorsController>/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
-
 		// POST api/<AdministratorsController>
 		[HttpPost]
-		public void Post([FromBody] string value)
+		public void Post([FromBody] AdministratorInputModel model)
 		{
-		}
-
-		// PUT api/<AdministratorsController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
-
-		// DELETE api/<AdministratorsController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
+			throw new NotImplementedException();
 		}
 	}
 }
