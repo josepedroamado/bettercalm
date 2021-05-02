5840197ce6a5f251191ce9d8558ceef542e5ae73
dscaffo <dscaffo@gmail.com>
2021-05-02 02:05:10 -0300

    Implemented get all administrators (WebAPI)

---
+++ server/BetterCalm/Model/AdministratorOutputModel.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace Model
+{
+	public class AdministratorOutputModel
+	{
+		public int Id { get; set; }
+		public string EMail { get; set; }
+
+		public AdministratorOutputModel(User user)
+		{
+			this.Id = user.Id;
+			this.EMail = user.EMail;
+		}
+	}
+}
