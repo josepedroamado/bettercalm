196b5c6b19de65cda071a99194a66f7be37e4ad0
pepe16694 <jose.pedro.amado@gmail.com>
2021-05-03 15:46:15 -0300

    Implemented Get in PsychologistsController (WebAPI)

--- server/BetterCalm/WebAPI/Controllers/PsychologistsController.cs
+++ server/BetterCalm/WebAPI/Controllers/PsychologistsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Model;
 using BLInterfaces;
 using WebAPI.Filters;
-using System;
+using System.Linq;
 
 namespace WebAPI.Controllers
 {
@@ -23,7 +23,10 @@ namespace WebAPI.Controllers
 		[HttpGet]
 		public IActionResult Get()
 		{
-			throw new NotImplementedException();
+			IEnumerable<PsychologistModel> models =
+				   this.psychologistLogic.GetAll().
+				   Select(psychologist => new PsychologistModel(psychologist));
+			return Ok(models);
 		}
 
 		[HttpGet("{id}")]
