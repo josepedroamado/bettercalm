be0f133b867cac5eb37d7f9ed0cd068c3f282b61
dscaffo <dscaffo@gmail.com>
2021-05-03 21:25:38 -0300

    Added validation test for user at domain level

---
+++ server/BetterCalm/Domain.Test/UserTest.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Test
+{
+	[TestClass]
+	public class UserTest
+	{
+		[TestMethod]
+		public void TestValidateOk()
+		{
+			User user = new User()
+			{
+				EMail = "a@a.com",
+				Name = "name",
+				Password = "password"
+			};
+
+			Assert.IsTrue(user.Validate());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidInputException))]
+		public void TestValidateWithoutEMail()
+		{
+			User user = new User()
+			{
+				Name = "name",
+				Password = "password"
+			};
+
+			Assert.IsFalse(user.Validate());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidInputException))]
+		public void TestValidateWithoutName()
+		{
+			User user = new User()
+			{
+				EMail = "a@a.com",
+				Password = "password"
+			};
+
+			Assert.IsFalse(user.Validate());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidInputException))]
+		public void TestValidateWithoutPassword()
+		{
+			User user = new User()
+			{
+				EMail = "a@a.com",
+				Name = "name"
+			};
+
+			Assert.IsFalse(user.Validate());
+		}
+	}
+}
