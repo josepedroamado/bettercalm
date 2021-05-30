cc3e9751f3162838108190f12f1327b4c1ee340f
dscaffo <dscaffo@gmail.com>
2021-05-29 22:13:40 -0300

    Added ContentTypeRepository with get method declaration (not implemented)

---
+++ server/BetterCalm/DataAccess/Repositories/ContentTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using System;
+
+namespace DataAccess.Repositories
+{
+	public class ContentTypeRepository : IContentTypeRepository
+	{
+		public ContentType Get(string name)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
