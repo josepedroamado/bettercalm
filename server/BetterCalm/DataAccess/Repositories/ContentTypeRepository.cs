6fbbc06fb6995dc1c0d9c3a83da7ac74094f6f62
dscaffo <dscaffo@gmail.com>
2021-05-29 22:18:29 -0300

    Added constructor to content type repository

--- server/BetterCalm/DataAccess/Repositories/ContentTypeRepository.cs
+++ server/BetterCalm/DataAccess/Repositories/ContentTypeRepository.cs
@@ -1,11 +1,21 @@
 ﻿using DataAccessInterfaces;
 using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace DataAccess.Repositories
 {
 	public class ContentTypeRepository : IContentTypeRepository
 	{
+		private DbContext context;
+		private DbSet<Content> contents;
+
+		public ContentTypeRepository(DbContext context)
+		{
+			this.context = context;
+			this.contents = context.Set<Content>();
+		}
+
 		public ContentType Get(string name)
 		{
 			throw new NotImplementedException();
