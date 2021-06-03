dd2d84ce7bf5394689449a0e112eefef42a025cc
dscaffo <dscaffo@gmail.com>
2021-06-03 20:24:22 -0300

    Added ImporterJSON and ImporterJSON.Test projects

---
+++ server/BetterCalm/ImporterJSON.Test/ImporterTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImporterJSON.Test
+{
+	[TestClass]
+	public class ImporterTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ server/BetterCalm/ImporterJSON/Importer.cs
@@ -0,0 +1,19 @@
+﻿using ImporterInterfaces;
+using ImporterModel;
+using System;
+
+namespace ImporterJSON
+{
+	public class Importer : IImporter
+	{
+		public string GetId()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ContentImport Import(string filePath)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
