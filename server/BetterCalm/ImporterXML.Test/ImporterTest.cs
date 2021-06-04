0b07da0ca7cf7511ee6889f4921bed951e61689d
dscaffo <dscaffo@gmail.com>
2021-06-03 22:26:26 -0300

    Added test cases for xml import

--- server/BetterCalm/ImporterXML.Test/ImporterTest.cs
+++ server/BetterCalm/ImporterXML.Test/ImporterTest.cs
@@ -1,4 +1,9 @@
+using ImporterInterfaces;
+using ImporterModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ImporterXML.Test
 {
@@ -6,8 +11,78 @@ namespace ImporterXML.Test
 	public class ImporterTest
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void GetId_IdIsOk_Fetched()
 		{
+			string expectedId = "XML";
+			IImporter importer = new Importer();
+			Assert.AreEqual(expectedId, importer.GetId());
+		}
+
+		[TestMethod]
+		public void Import_IsOk_Fetched()
+		{
+			PlaylistImport playlist = new PlaylistImport()
+			{
+				Name = "Trending",
+				Description = "Trending music",
+				Categories = new int[] { 1002 }
+			};
+			ContentImport content = new ContentImport()
+			{
+				Name = "Beat It extended",
+				ContentLength = "00:06:30",
+				ArtistName = "Michael Jackson",
+				ImageUrl = "http://images.com/image3.jpg",
+				ContentUrl = "http://audio.com/audio3.mp3",
+				Categories = new int[] { 1002 },
+				ContentType = "audio",
+				Playlists = new PlaylistImport[] { playlist }
+			};
+			List<ContentImport> expectedContents = new List<ContentImport>()
+			{
+				content
+			};
+			string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+			string filePath = Path.Combine(basePath, "importTest.xml");
+
+			IImporter importer = new Importer();
+			IEnumerable<ContentImport> imported = importer.Import(filePath);
+
+			Assert.IsTrue(imported.SequenceEqual(expectedContents));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(FileNotFoundException))]
+		public void Import_FileNotFound_Failed()
+		{
+			PlaylistImport playlist = new PlaylistImport()
+			{
+				Name = "Trending",
+				Description = "Trending music",
+				Categories = new int[] { 1002 }
+			};
+			ContentImport content = new ContentImport()
+			{
+				Name = "Beat It extended",
+				ContentLength = "00:06:30",
+				ArtistName = "Michael Jackson",
+				ImageUrl = "http://images.com/image3.jpg",
+				ContentUrl = "http://audio.com/audio3.mp3",
+				Categories = new int[] { 1002 },
+				ContentType = "audio",
+				Playlists = new PlaylistImport[] { playlist }
+			};
+			List<ContentImport> expectedContents = new List<ContentImport>()
+			{
+				content
+			};
+			string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+			string filePath = Path.Combine(basePath, "notfound.xml");
+
+			IImporter importer = new Importer();
+			IEnumerable<ContentImport> imported = importer.Import(filePath);
+
+			Assert.IsFalse(imported.SequenceEqual(expectedContents));
 		}
 	}
 }
