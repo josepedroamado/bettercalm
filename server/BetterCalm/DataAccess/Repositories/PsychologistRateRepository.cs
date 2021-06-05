47be498be18e3e6efcf4e029556c67807fff4e13
pepe16694 <jose.pedro.amado@gmail.com>
2021-06-05 12:15:47 -0300

    Added test cases for PsychologistRateRepository, added needed interface and implementation with required methods.

---
+++ server/BetterCalm/DataAccess/Repositories/PsychologistRateRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Repositories
+{
+    public class PsychologistRateRepository : IPsychologistRateRepository
+    {
+        private DbContext context;
+        private DbSet<PsychologistRate> rates;
+
+        public PsychologistRateRepository(DbContext context)
+        {
+            this.context = context;
+            this.rates = context.Set<PsychologistRate>();
+        }
+
+        public PsychologistRate Get(int rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PsychologistRate> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
