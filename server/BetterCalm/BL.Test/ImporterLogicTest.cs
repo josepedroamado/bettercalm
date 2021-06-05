8ca649efe067824456b010b769e7e860fe4bf942
dscaffo <dscaffo@gmail.com>
2021-06-05 20:26:39 -0300

    Added new test cases for importer logic

--- server/BetterCalm/BL.Test/ImporterLogicTest.cs
+++ server/BetterCalm/BL.Test/ImporterLogicTest.cs
@@ -29,7 +29,7 @@ namespace BL.Test
 			};
 			Category category = new Category()
 			{
-				Id = 1,
+				Id = 1002,
 				Name = "music"
 			};
 			Content content = new Content()
@@ -88,7 +88,7 @@ namespace BL.Test
 			};
 			Category category = new Category()
 			{
-				Id = 1,
+				Id = 1002,
 				Name = "music"
 			};
 			Content content = new Content()
@@ -148,7 +148,7 @@ namespace BL.Test
 			};
 			Category category = new Category()
 			{
-				Id = 1,
+				Id = 1002,
 				Name = "music"
 			};
 			Content content = new Content()
@@ -208,7 +208,7 @@ namespace BL.Test
 			};
 			Category category = new Category()
 			{
-				Id = 1,
+				Id = 1002,
 				Name = "music"
 			};
 			Content content = new Content()
@@ -241,6 +241,66 @@ namespace BL.Test
 			string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
 			string filePath = Path.Combine(basePath, "notfound.xml");
 			string fileType = "XML";
+
+			importerLogic.Import(fileType, filePath);
+
+			List<Content> obtainedContents = contentLogic.GetContents().ToList();
+
+			Assert.IsTrue(obtainedContents.Any(obtainedContent => {
+				obtainedContent.Id = 0;
+				return obtainedContent.Equals(content);
+			}));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ImporterNotFoundException))]
+		public void Import_InvalidImporter_ImporterNotFound()
+		{
+			Playlist playlist = new Playlist()
+			{
+				Name = "Trending",
+				Description = "Trending music"
+			};
+			ContentType contentType = new ContentType()
+			{
+				Id = 1,
+				Name = "audio"
+			};
+			Category category = new Category()
+			{
+				Id = 1002,
+				Name = "music"
+			};
+			Content content = new Content()
+			{
+				Name = "Beat It extended",
+				ContentLength = new TimeSpan(00, 06, 30),
+				ArtistName = "Michael Jackson",
+				ImageUrl = "http://images.com/image3.jpg",
+				ContentUrl = "http://audio.com/audio3.mp3",
+				Categories = new List<Category>() { category },
+				ContentType = contentType,
+				PlayLists = new Playlist[] { playlist }
+			};
+
+			List<Content> contents = new List<Content>() { content };
+
+			Mock<IContentRepository> contentRepositoryMock = new Mock<IContentRepository>(MockBehavior.Strict);
+			contentRepositoryMock.Setup(m => m.Add(It.IsAny<Content>()));
+			contentRepositoryMock.Setup(m => m.GetAll()).Returns(contents);
+
+			Mock<IPlaylistRepository> playlistRepositoryMock = new Mock<IPlaylistRepository>(MockBehavior.Strict);
+			playlistRepositoryMock.Setup(m => m.Get(playlist.Id)).Throws(new NotFoundException(playlist.Id.ToString()));
+
+			Mock<ICategoryRepository> categoryRepositoryMock = new Mock<ICategoryRepository>(MockBehavior.Strict);
+			categoryRepositoryMock.Setup(m => m.Get(category.Id)).Returns(category);
+
+			ContentLogic contentLogic = new ContentLogic(contentRepositoryMock.Object, playlistRepositoryMock.Object, categoryRepositoryMock.Object);
+			ImporterLogic importerLogic = new ImporterLogic(contentLogic);
+
+			string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+			string filePath = Path.Combine(basePath, "notfound.xml");
+			string fileType = "CSV";
 
 			importerLogic.Import(fileType, filePath);
 
