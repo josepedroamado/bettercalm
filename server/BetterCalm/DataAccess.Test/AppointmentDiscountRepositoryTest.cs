15c57b4ce4654a57aae0920a6c48222002bf4666
pepe16694 <jose.pedro.amado@gmail.com>
2021-06-05 18:03:38 -0300

    Moved AppointmentDiscountRepository to the correct folder and fixed the namespace. Added test case for GetAll when discounts exist and added needed constructor and parameters to implementation.

--- server/BetterCalm/DataAccess.Test/AppointmentDiscountRepositoryTest.cs
+++ server/BetterCalm/DataAccess.Test/AppointmentDiscountRepositoryTest.cs
@@ -1,8 +1,13 @@
 ﻿using DataAccess.Context;
+using DataAccess.Repositories;
+using Domain;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 
 namespace DataAccess.Test
 {
@@ -32,6 +37,46 @@ namespace DataAccess.Test
         public void TestCleanup()
         {
             this.context.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void GetAll_DiscountsExist_Fetched()
+        {
+            List<AppointmentDiscount> expectedDiscounts = GetAllExpectedDiscounts();
+
+            foreach (AppointmentDiscount discount in expectedDiscounts)
+            {
+                this.context.Add(discount);
+            }
+            this.context.SaveChanges();
+            AppointmentDiscountRepository appointmentDiscountRepository = new AppointmentDiscountRepository(this.context);
+
+            IEnumerable<AppointmentDiscount> obtainedDiscounts = appointmentDiscountRepository.GetAll();
+            Assert.IsTrue(expectedDiscounts.SequenceEqual(obtainedDiscounts));
+        }
+
+        private List<AppointmentDiscount> GetAllExpectedDiscounts()
+        {
+            AppointmentDiscount fifteen = new AppointmentDiscount()
+            {
+                Id = 1,
+                Discount = 15
+            };
+            AppointmentDiscount twentyFive = new AppointmentDiscount()
+            {
+                Id = 2,
+                Discount = 25
+            };
+            AppointmentDiscount fifty = new AppointmentDiscount()
+            {
+                Id = 3,
+                Discount = 50
+            };
+            List<AppointmentDiscount> discounts = new List<AppointmentDiscount>()
+            {
+                fifteen, twentyFive, fifty
+            };
+            return discounts;
         }
     }
 }
--- server/BetterCalm/DataAccess/Repositories/AppointmentDiscountRepository.cs
+++ server/BetterCalm/DataAccess/Repositories/AppointmentDiscountRepository.cs
@@ -1,12 +1,22 @@
 ﻿using DataAccessInterfaces;
 using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 
-namespace DataAccess
+namespace DataAccess.Repositories
 {
     public class AppointmentDiscountRepository : IAppointmentDiscountRepository
     {
+        private DbContext context;
+        private DbSet<AppointmentDiscount> discounts;
+
+        public AppointmentDiscountRepository(DbContext context)
+        {
+            this.context = context;
+            this.discounts = context.Set<AppointmentDiscount>();
+        }
+
         public AppointmentDiscount Get(int? discount)
         {
             throw new NotImplementedException();
