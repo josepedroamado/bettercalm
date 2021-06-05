fd408e60872f82cab51181620171dff97dff7c17
dscaffo <dscaffo@gmail.com>
2021-06-05 19:17:28 -0300

    Added constructor to importer logic, as needed to create the test cases

--- server/BetterCalm/BL/ImporterLogic.cs
+++ server/BetterCalm/BL/ImporterLogic.cs
@@ -5,6 +5,12 @@ namespace BL
 {
 	public class ImporterLogic : IImporterLogic
 	{
+		private IContentLogic contentLogic;
+		public ImporterLogic(IContentLogic contentLogic)
+		{
+			this.contentLogic = contentLogic;
+		}
+
 		public void Import(string type, string filePath)
 		{
 			throw new NotImplementedException();
