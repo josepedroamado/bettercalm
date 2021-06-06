b597d9a3f00cd5ba0b1544dd3c75517a3eb68fbe
dscaffo <dscaffo@gmail.com>
2021-06-05 21:07:50 -0300

    Minor fix for importer not found exception

--- server/BetterCalm/Domain/Exceptions/ImporterNotFoundException.cs
+++ server/BetterCalm/Domain/Exceptions/ImporterNotFoundException.cs
@@ -4,6 +4,6 @@ namespace Domain.Exceptions
 {
 	public class ImporterNotFoundException : Exception
 	{
-		public ImporterNotFoundException(string type, string path) : base($"Importer ${type} was not found at {path}.") {}
+		public ImporterNotFoundException(string type, string path) : base($"Importer {type} was not found at {path}.") {}
 	}
 }
