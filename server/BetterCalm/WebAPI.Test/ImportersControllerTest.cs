8074f40f7fa2b37b44e79eab51efd0d11318ed4f
dscaffo <dscaffo@gmail.com>
2021-06-05 21:10:30 -0300

    Added test cases for importers controller

--- server/BetterCalm/WebAPI.Test/ImportersControllerTest.cs
+++ server/BetterCalm/WebAPI.Test/ImportersControllerTest.cs
@@ -1,11 +1,13 @@
 ﻿using BLInterfaces;
 using Domain;
+using Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using WebAPI.Controllers;
 
@@ -14,6 +16,113 @@ namespace WebAPI.Test
     [TestClass]
     public class ImportersControllerTest
     {
+        [TestMethod]
+        public void Post_JSON_OK()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "importTest.json");
+            string fileType = "JSON";
 
+            ImporterInputModel inputModel = new ImporterInputModel()
+            {
+                FilePath = filePath,
+                Type = fileType
+            };
+
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Import(fileType, filePath));
+            ImportersController controller = new ImportersController(mock.Object);
+            IActionResult result = controller.Post(inputModel);
+            Assert.IsTrue(result is NoContentResult);
+        }
+
+        [TestMethod]
+        public void Post_XML_OK()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "importTest.xml");
+            string fileType = "XML";
+
+            ImporterInputModel inputModel = new ImporterInputModel()
+            {
+                FilePath = filePath,
+                Type = fileType
+            };
+
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Import(fileType, filePath));
+            ImportersController controller = new ImportersController(mock.Object);
+            IActionResult result = controller.Post(inputModel);
+            Assert.IsTrue(result is NoContentResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void Post_JSON_NotFound()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "notfound.json");
+            string fileType = "JSON";
+            int expectedStatusCode = 400;
+
+            ImporterInputModel inputModel = new ImporterInputModel()
+            {
+                FilePath = filePath,
+                Type = fileType
+            };
+
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Import(fileType, filePath)).Throws(new FileNotFoundException());
+            ImportersController controller = new ImportersController(mock.Object);
+            IActionResult result = controller.Post(inputModel);
+            StatusCodeResult contentResult = result as StatusCodeResult;
+            Assert.AreEqual(expectedStatusCode, contentResult.StatusCode);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void Post_XML_NotFound()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "notfound.xml");
+            string fileType = "XML";
+            int expectedStatusCode = 400;
+
+            ImporterInputModel inputModel = new ImporterInputModel()
+            {
+                FilePath = filePath,
+                Type = fileType
+            };
+
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Import(fileType, filePath)).Throws(new FileNotFoundException());
+            ImportersController controller = new ImportersController(mock.Object);
+            IActionResult result = controller.Post(inputModel);
+            StatusCodeResult contentResult = result as StatusCodeResult;
+            Assert.AreEqual(expectedStatusCode, contentResult.StatusCode);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ImporterNotFoundException))]
+        public void Post_InvalidImporter_ImporterNotFound()
+        {
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "importTest.csv");
+            string fileType = "CSV";
+            int expectedStatusCode = 500;
+
+            ImporterInputModel inputModel = new ImporterInputModel()
+            {
+                FilePath = filePath,
+                Type = fileType
+            };
+
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Import(fileType, filePath)).Throws(new ImporterNotFoundException(fileType, ""));
+            ImportersController controller = new ImportersController(mock.Object);
+            IActionResult result = controller.Post(inputModel);
+            StatusCodeResult contentResult = result as StatusCodeResult;
+            Assert.AreEqual(expectedStatusCode, contentResult.StatusCode);
+        }
     }
 }
