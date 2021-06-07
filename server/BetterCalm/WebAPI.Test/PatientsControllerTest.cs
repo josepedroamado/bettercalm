e6607025afbac0f08967a9e0962d7968035ae610
pepe16694 <jose.pedro.amado@gmail.com>
2021-06-07 15:35:17 -0300

    Created Patients Controller and Test classes. Created test case for GetApproveDiscounts when patients exist and meet the requirements.

---
+++ server/BetterCalm/WebAPI.Test/PatientsControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using BLInterfaces;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Controllers;
+
+namespace WebAPI.Test
+{
+    [TestClass]
+    public class PatientsControllerTest
+    {
+        [TestMethod]
+        public void GetApproveDiscounts_PatientsExistAndMeetRequirements_Fetched()
+        {
+            IEnumerable<Patient> expectedPatients = GetApproveDiscountsExpectedPatients();
+            Mock<IPatientLogic> mock = new Mock<IPatientLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.GetAllWithoutDiscountAndRequiredAppointmentQuantity()).Returns(expectedPatients);
+            PatientsController controller = new PatientsController(mock.Object);
+
+            IActionResult result = controller.Get();
+            OkObjectResult objectResult = result as OkObjectResult;
+            IEnumerable<Patient> obtainedPatients = objectResult.Value as IEnumerable<Patient>;
+
+            mock.VerifyAll();
+            Assert.IsTrue(expectedPatients.SequenceEqual(obtainedPatients));
+        }
+
+        private List<Patient> GetApproveDiscountsExpectedPatients()
+        {
+            Patient johnDoe = new Patient()
+            {
+                BirthDate = new DateTime(1993, 11, 15),
+                Email = "john.doe@gmail.com",
+                FirstName = "John",
+                LastName = "Doe",
+                Id = 1,
+                Phone = "46465551256",
+                AppointmentQuantity = 1
+            };
+            Patient janeDoe = new Patient()
+            {
+                BirthDate = new DateTime(1993, 11, 15),
+                Email = "jane.doe@gmail.com",
+                FirstName = "Jane",
+                LastName = "Doe",
+                Id = 2,
+                Phone = "36325551478",
+                AppointmentQuantity = 4
+            };
+            return new List<Patient>() { johnDoe, janeDoe };
+        }
+    }
+}
---
+++ server/BetterCalm/WebAPI/Controllers/PatientsController.cs
@@ -0,0 +1,25 @@
+﻿using BLInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WebAPI.Filters;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/patients")]
+    [ApiController]
+    [AuthorizationFilter("Administrator")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientLogic patientLogic;
+
+        public PatientsController(IPatientLogic patientLogic)
+        {
+            this.patientLogic = patientLogic;
+        }
+        [HttpGet("/approvediscounts")]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
