1b0902bc40a248e72078f08ce3eddf811014d059
dscaffo <dscaffo@gmail.com>
2021-06-07 23:13:50 -0300

    Updating app module

--- webclient/better-calm-webapp/src/app/app.module.ts
+++ webclient/better-calm-webapp/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CardComponent } from './card/card.component';
 import { ContentsComponent } from './contents/contents.component';
+import { ButtonComponent } from './button/button.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardComponent,
-    ContentsComponent
+    ContentsComponent,
+    ButtonComponent,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule,
