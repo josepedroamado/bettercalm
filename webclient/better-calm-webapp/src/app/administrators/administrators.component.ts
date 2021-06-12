a025315347170542599429f96370364dffbf4516
pepe16694 <jose.pedro.amado@gmail.com>
2021-06-12 20:29:55 -0300

    Created administrator model, implemented administrator view

--- webclient/better-calm-webapp/src/app/administrators/administrators.component.ts
+++ webclient/better-calm-webapp/src/app/administrators/administrators.component.ts
@@ -1,4 +1,7 @@
+import { Administrator } from './../model/administrator';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdministratorsService } from '../services/administrators/administrators.service';
 
 @Component({
   selector: 'app-administrators',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrators.component.scss']
 })
 export class AdministratorsComponent implements OnInit {
-
-  constructor() { }
+  administrators:Administrator[] = [];
+  
+  constructor(private router: Router, private administratorsService: AdministratorsService) { }
 
   ngOnInit(): void {
+    this.updateAdministrators();
   }
 
+  private updateAdministrators(){
+    this.administratorsService.getAll().subscribe((contents) => this.setPsychologists(contents), console.error);
+  }
+  private setPsychologists(administrators:Administrator[]){
+    this.administrators = administrators;
+  }
+
+  goToAddView(){
+    this.router.navigate(['/administrator-add'])
+  }
+
+  removeAdministrator(administrator: number){
+    this.administratorsService.remove(administrator).subscribe();
+    this.updateAdministrators();
+  }
 }
