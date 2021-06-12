8148a4c3c71aa9c53bafcf45eaa321718286846e
dscaffo <dscaffo@gmail.com>
2021-06-12 17:49:02 -0300

    Added get method to contents service

--- webclient/better-calm-webapp/src/app/services/contents/contents.service.ts
+++ webclient/better-calm-webapp/src/app/services/contents/contents.service.ts
@@ -6,7 +6,9 @@ import { map, catchError } from 'rxjs/operators';
 import { Content } from 'src/app/domain/content';
 import { ContentBasicInfo } from 'src/app/model/content-basic-info';
 import { ContentBasicInfoConverter } from 'src/app/model/content-basic-info-converter';
+import { ModelContent } from 'src/app/model/model-content';
 import { BaseService } from '../common/base-service';
+import { ModelContentConverter } from 'src/app/model/model-content-converter';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +26,18 @@ export class ContentsService extends BaseService{
       .pipe(catchError(this.handleError), map(this.convertBasicInfoContents));
   }
 
+  public get(id:string): Observable<Content> {
+    let getContentUrl:string = this.target_url+"/"+id;
+    return this.http
+      .get<ModelContent>(getContentUrl)
+      .pipe(catchError(this.handleError), map(this.convertModelContent));
+  }
+
   private convertBasicInfoContents(contentsBasicInfo:ContentBasicInfo[]):Content[]{
     return contentsBasicInfo.map(content => ContentBasicInfoConverter.GetDomainContent(content));
+  }
+
+  private convertModelContent(modelContent:ModelContent):Content{
+    return ModelContentConverter.GetDomainContent(modelContent);
   }
 }
