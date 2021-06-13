6e6be724c079526c21ed4fb780ecf25eacd38bbb
dscaffo <dscaffo@gmail.com>
2021-06-13 18:08:30 -0300

    Fix for auth options

--- webclient/better-calm-webapp/src/app/services/common/base-service.ts
+++ webclient/better-calm-webapp/src/app/services/common/base-service.ts
@@ -4,17 +4,19 @@ import { throwError } from "rxjs";
 
 @Injectable()
 export class BaseService{
-  public authOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem("token") ?? ""
-    })
-  };
-  
     constructor(public http: HttpClient) { }
     
     public handleError(errorRequest: any) {
       console.error(errorRequest);
       return throwError(errorRequest.error || errorRequest.message);
     }
+
+  public getAuthOptions(){
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token") ?? ""
+      })
+    };
+  }
 }
