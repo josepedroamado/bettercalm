847e08ca5f9e3b74dc73ed6ec722a6712f459418
dscaffo <dscaffo@gmail.com>
2021-06-13 01:49:57 -0300

    Added getcontents to playlists service

--- webclient/better-calm-webapp/src/app/services/playlists/playlists.service.ts
+++ webclient/better-calm-webapp/src/app/services/playlists/playlists.service.ts
@@ -2,7 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { Content } from 'src/app/domain/content';
 import { Playlist } from 'src/app/domain/playlist';
+import { ContentBasicInfo } from 'src/app/model/content-basic-info';
+import { ContentBasicInfoConverter } from 'src/app/model/content-basic-info-converter';
 import { PlaylistBasicInfo } from 'src/app/model/playlist-basic-info';
 import { PlaylistBasicInfoConverter } from 'src/app/model/playlist-basic-info-converter';
 import { environment } from 'src/environments/environment';
@@ -24,7 +27,18 @@ export class PlaylistsService extends BaseService {
       .pipe(catchError(this.handleError), map(this.convertBasicInfoPlaylists));
   }
 
+  public getContents(id:number): Observable<Content[]> {
+    let url = this.target_url+'/'+id+'/contents';
+    return this.http
+      .get<ContentBasicInfo[]>(url)
+      .pipe(catchError(this.handleError), map(this.convertBasicInfoContents));
+  }
+
   private convertBasicInfoPlaylists(basicInfo:PlaylistBasicInfo[]):Playlist[]{
     return basicInfo.map(basic => PlaylistBasicInfoConverter.GetDomainPlaylist(basic));
+  }
+
+  private convertBasicInfoContents(contentsBasicInfo:ContentBasicInfo[]):Content[]{
+    return contentsBasicInfo.map(content => ContentBasicInfoConverter.GetDomainContent(content));
   }
 }
