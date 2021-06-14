57d0d5891e928ad76605974cb63e9a9be866b968
pepe16694 <jose.pedro.amado@gmail.com>
2021-06-14 20:30:24 -0300

    Fixed wrong formControlName for the image when adding a new playlist, made content type titlecase in content card, fixed time stamps not showing all numbers, added date format pipe to the date when returning the appointment information,

--- webclient/better-calm-webapp/src/app/model/time-stamp-converter.ts
+++ webclient/better-calm-webapp/src/app/model/time-stamp-converter.ts
@@ -2,6 +2,27 @@ import { TimeStamp } from "./time-stamp"
 
 export class TimeStampConverter{
     public static GetDomainTimeStamp(ts:TimeStamp):string{
-        return `${ts.hours}:${ts.minutes}:${ts.seconds}`;
+        let hours = `${ts.hours}`;
+        if (ts.hours < 10){
+            hours = `0${ts.hours}`;
+        }
+        if (ts.hours == 0){
+            hours = `0${ts.hours}`;
+        }
+        let minutes = `${ts.minutes}`;
+        if (ts.minutes < 10){
+            minutes = `0${ts.minutes}`;
+        }
+        if (ts.minutes == 0){
+            minutes = `0${ts.minutes}`;
+        }
+        let seconds = `${ts.seconds}`;
+        if (ts.minutes < 10){
+            seconds = `0${ts.seconds}`;
+        }
+        if (ts.seconds == 0){
+            seconds = `0${ts.seconds}`;
+        }
+        return `${hours}:${minutes}:${seconds}`;
     }
 }
